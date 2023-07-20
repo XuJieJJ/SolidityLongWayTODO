73e1fc5a945454db63c632c178c9bf7f1b4557a3
demojj <3084966130@qq.com>
2023-07-20 18:00:52 +0800

    1

--- SolidityByExamole/UniswapV2/code/UniswapV2/libraries/SafeMath.sol
+++ SolidityByExamole/UniswapV2/code/UniswapV2/libraries/SafeMath.sol
@@ -12,6 +12,6 @@ library SafeMath{
     }
 
     function mul(uint x ,uint y )internal pure returns (uint z ){
-        require(y==0||(z=x*y)/y==x,"mul overflow");
+        require(y==0||(z = x*y)/y==x,"mul overflow");
     }
 }
